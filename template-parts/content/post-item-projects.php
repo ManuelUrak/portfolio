fc8ce400ad1e880c725622efaa0657f8f76ad2a3
Manuel Urak <manuel.urak88@gmail.com>
2025-02-05 14:14:29 +0100

    Modified post-items HTML structure to increase UX

--- template-parts/content/post-item-projects.php
+++ template-parts/content/post-item-projects.php
@@ -1,12 +1,14 @@
-<div class="projects-content--inner">
-    <div class="img-container img-container--contain lazy">
-        <div class="img-container__inner">
-            <?php the_post_thumbnail('medium'); ?>
+<a class="no-style" href="<?php echo the_permalink(); ?>">
+    <div class="projects-content--inner">
+        <div class="img-container img-container--contain lazy">
+            <div class="img-container__inner">
+                <?php the_post_thumbnail('medium'); ?>
+            </div>
+        </div>
+        <div class="content">
+            <h3><?php echo the_title(); ?></h3>
+            <p><?php echo the_excerpt(); ?></p>
+            <span class="button button--primary">Zum Projekt</span>
         </div>
     </div>
-    <div class="content">
-        <h3><?php echo the_title(); ?></h3>
-        <p><?php echo the_excerpt(); ?></p>
-        <a href="<?php echo the_permalink(); ?>" class="button button--primary">Zum Projekt</a>
-    </div>
-</div>
+</a>
